8f6231992820ecd24313542fcdd9e94d7d7cc8cf
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-07-28 20:48:39 -0300

    Included Recycle view in activity layout & Created business card item for recycler view

---
+++ app/src/main/java/me/dio/vinicius/businesscard/App.kt
@@ -0,0 +1,6 @@
+package me.dio.vinicius.businesscard
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/me/dio/vinicius/businesscard/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package me.dio.vinicius.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.vinicius.businesscard.R
+import me.dio.vinicius.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
