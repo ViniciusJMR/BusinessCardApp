92d0a14ac9a25e2c51d0f7c05aeecc8209eef54d
ViniciusJMR <viniciusdejesus605@gmail.com>
2022-12-13 13:48:28 -0300

    Add and Edit card is now using spinner for the background color selection

--- app/src/main/java/me/dio/vinicius/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/me/dio/vinicius/businesscard/ui/AddBusinessCardActivity.kt
@@ -1,6 +1,10 @@
 package me.dio.vinicius.businesscard.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -11,18 +15,46 @@ import me.dio.vinicius.businesscard.domain.BusinessCard
 import me.dio.vinicius.businesscard.viewmodel.MainViewModel
 import me.dio.vinicius.businesscard.viewmodel.MainViewModelFactory
 
-class AddBusinessCardActivity : AppCompatActivity() {
+class AddBusinessCardActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
 
     private val binding by lazy {ActivityAddBusinessCardBinding.inflate(layoutInflater)}
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory((application as App).repository)
     }
+    private val colorMap =
+        mapOf("White" to R.color.white, "Black" to R.color.black
+            , "Purple" to R.color.purple_200, "Teal" to R.color.teal_200 )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.card = BusinessCard()
         binding.activity = this
+
+        setupSpinner()
+    }
+
+    private fun setupSpinner(){
+        ArrayAdapter.createFromResource(
+            this,
+                R.array.color_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.sNewCardColor.adapter = adapter
+        }
+        binding.sNewCardColor.onItemSelectedListener = this
+    }
+
+
+    override fun onItemSelected(parent: AdapterView<*>?, v: View?, pos: Int, id: Long) {
+        val item = parent?.getItemAtPosition(pos) as String
+
+        binding.card?.backgroundColor = resources.getString(colorMap[item]!!)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        binding.card?.backgroundColor = resources.getString(colorMap["white"]!!)
     }
 
     fun confirmCard(){
--- app/src/main/java/me/dio/vinicius/businesscard/ui/EditBusinessCardActivity.kt
+++ app/src/main/java/me/dio/vinicius/businesscard/ui/EditBusinessCardActivity.kt
@@ -1,6 +1,9 @@
 package me.dio.vinicius.businesscard.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -10,12 +13,16 @@ import me.dio.vinicius.businesscard.databinding.ActivityEditBusinessCardBinding
 import me.dio.vinicius.businesscard.viewmodel.MainViewModel
 import me.dio.vinicius.businesscard.viewmodel.MainViewModelFactory
 
-class EditBusinessCardActivity : AppCompatActivity() {
+class EditBusinessCardActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private val binding by lazy { ActivityEditBusinessCardBinding.inflate(layoutInflater)}
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory((application as App).repository)
     }
     private var id: Int = 0
+
+    private val colorMap =
+        mapOf("White" to R.color.white, "Black" to R.color.black
+            , "Purple" to R.color.purple_200, "Teal" to R.color.teal_200 )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +30,8 @@ class EditBusinessCardActivity : AppCompatActivity() {
         binding.activity = this
 
         fillInfo()
+        setupSpinner()
     }
-
 
     private fun fillInfo(){
         id = intent.extras?.getInt("id")!!
@@ -33,9 +40,32 @@ class EditBusinessCardActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupSpinner(){
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.color_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.sUpdateCardColor.adapter = adapter
+        }
+        binding.sUpdateCardColor.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, v: View?, pos: Int, id: Long) {
+        val item = parent?.getItemAtPosition(pos) as String
+
+        binding.card?.backgroundColor = resources.getString(colorMap[item]!!)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        binding.card?.backgroundColor = resources.getString(colorMap["white"]!!)
+    }
+
     fun confirmCard() {
         mainViewModel.update(binding.card!!)
         Snackbar.make(binding.root, R.string.label_show_update_success, Snackbar.LENGTH_SHORT).show()
         finish()
     }
+
 }
